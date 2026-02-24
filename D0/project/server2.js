d55042e8414ab53c412fb1b7fb992da9f9105a4b
Anya Jha <anyamrendrajha@gmail.com>
2026-02-24 12:08:09 +0530

    Create server2.js

---
+++ D0/project/server2.js
@@ -0,0 +1,42 @@
+const fs=require("fs");
+const http=require("http");
+
+
+
+const server=http.createServer((req,res)=>
+{
+    if(req.url==="/")
+    {
+        const html=fs.readFileSync("home.html");
+        res.writeHead(200,{"content-type":"text/html"});
+        res.end(html);
+    }
+
+    else if(req.url==="/profile")
+    {
+        const html=fs.readFileSync("profile.html", "utf-8");
+        const newHtml=html.replace("{{name}}", "Anya");
+
+        res.writeHead(200,{"content-type":"text/html"});
+        res.end(newHtml);
+    }
+
+    else if(req.url==="/about")
+    {
+        const html=fs.readFileSync("about.html");
+        res.writeHead(200,{"content-type":"text/html"});
+        res.end(html);
+    }
+
+    else if(req.url==="/contact")
+    {
+        const html=fs.readFileSync("contact.html");
+        res.writeHead(200,{"content-type":"text/html"});
+        res.end(html);
+    }
+});
+
+server.listen(8000, ()=>
+{
+    console.log("http://localhost:8000/");
+});
